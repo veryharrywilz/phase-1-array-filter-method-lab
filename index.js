dec58bdf8536c36835665cec4a0e06e0175bbd73
Harrison <harriwilson5534@gmail.com>
2022-09-30 14:24:42 -0400

    commit tree 007e37544dee3c9ebd76ffa6019a20f738a3bfb1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+
+function findMatching(nameList, name) {
+    let newArray = nameList.filter(key => {
+        return key.toLowerCase() === name.toLowerCase()
+        })
+    return newArray}
+
+let nameList = ['tommy', 'bob', 'pauly', 'Jimbo', 'piKachu', "tommy", "tommy", "tommy"]
+
+//console.log(findMatching(nameList, "tommy"))
+
+
+function fuzzyMatch(nameList, letters) {
+    let newArray = nameList.filter(key => {
+        return key[0].toLowerCase() === letters[0].toLowerCase()
+    })
+    return newArray
+}
+
+console.log(fuzzyMatch(nameList, 'jim'))
+
+
+function matchName(object, hometownString) {
+    let newArray = object.filter(key => {
+        return key.name.toLowerCase() === hometownString.toLowerCase()
+        })
+    return newArray
+}
